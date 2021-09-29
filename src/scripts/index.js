af6d73a4c171a2f60734cb3c672c30907f4550c1
Adam Wrobel <adam.marek.wrobel@gmail.com>
2021-09-30 01:37:22 +0200

    instructions for the task added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,16 @@
 import '../styles/main.scss';
 import "babel-polyfill";
 
+/* 
+Instructions:
+Look at the form on HTML. In the index.js file and create a post request on submit.
+Requirements:
+Use fetch to post data on fake server https://my-json-server.typicode.com/typicode/demo/posts
+Use async await syntax.
+When resolving your promise, print the raw JSON to the console.
+Alert the user once they have submitted the form.
+*/
+
 
 // \/ All of your javascript should go here \/
 
